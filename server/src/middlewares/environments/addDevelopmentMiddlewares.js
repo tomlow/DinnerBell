d33d52ac71d7e8b77a6af3c4405edc96c75f51e3
Tom Low <thomaslsternberg@gmail.com>
2021-02-12 16:07:33 -0500

    fixed nodeEnv references

--- server/src/middlewares/environments/addDevelopmentMiddlewares.js
+++ server/src/middlewares/environments/addDevelopmentMiddlewares.js
@@ -1,7 +1,7 @@
 import config from "../../config.js";
 
 const addDevelopmentMiddlewares = async (app) => {
-  if (config.nodeEnv === "development") {
+  if (config.env === "development") {
     const { default: errorHandler } = await import("../errorHandler.js");
     app.use(errorHandler());
   }
