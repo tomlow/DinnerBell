d9bcc816c63635b41ad8c91a9858e615c6ddc1d5
Tom Low <thomaslsternberg@gmail.com>
2021-02-12 15:10:05 -0500

    recipes now return with parsed html summaries

--- client/src/components/recipes/RecipeTile.js
+++ client/src/components/recipes/RecipeTile.js
@@ -6,7 +6,7 @@ const RecipeTile = ({ recipe }) => {
       <img src={recipe.image} alt="tasty beef stroganoff" className="card__image" />
       <div className="card__content-container">
         <h3 className="card__title">{recipe.title}</h3>
-        <p className="card__description">This is a fun and informative description of this recipe.</p>
+        <p dangerouslySetInnerHTML={{ __html: recipe.summary }} />
         <div className="card__button-container">
           <button className="card__btn">Save Recipe</button>
         </div>
