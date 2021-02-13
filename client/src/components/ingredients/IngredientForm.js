b7aa5f4ca2412e6a6c8d206bf7e51e8554f0bc90
Tom Low <thomaslsternberg@gmail.com>
2021-02-13 17:59:02 -0500

    WIP ingredient form post

--- client/src/components/ingredients/IngredientForm.js
+++ client/src/components/ingredients/IngredientForm.js
@@ -6,26 +6,49 @@ const IngredientForm = (props) => {
     name: ""
   })
 
-  const handleInputChange = async (event) => {
+  const postIngredient = async (formPayload) => {
+    const response = await fetch('/api/v1/ingredients', {
+      method: "POST",
+      headers: new Headers({
+        "Content-Type": "application/json"
+      }),
+      body: JSON.stringify(formPayload)
+    })
+    if (!response.ok) {
+      if (response.status === 422) {
+        const body = await response.json()
+        const newErrors = translateServerErrors(body.errors)
+        return setErrors(newErrors)
+      } else {
+        const errorMessage = `${response.status} (${response.statusText})`
+        const error = new Error(errorMessage)
+        throw error
+      }
+    }
+    else {
+      setErrors([])
+    }
+  }
+
+  const handleInputChange = (event) => {
     setIngredientRecord({
       ...ingredientRecord,
       [event.currentTarget.name]: event.currentTarget.value
     })
-    try {
-      const response = await fetch(`/ingredients/autocomplete/${ingredientRecord.name}`)
+  }
 
-    } catch (error) {
-
-    }
-
+  const onSubmitHandler = (event) => {
+    event.preventDefault()
+    postIngredient(ingredientRecord)
+    setIngredientRecord({ name: "" })
   }
 
   return <div>
-    <form>
+    <form onSubmit={onSubmitHandler}>
       <label>Ingredient Name:
         <input type="text" id="name" name="name" value={ingredientRecord.name} onChange={handleInputChange} />
       </label>
-      <input className="button" type="submit" value="Put it in the Pantry" />
+      <input className="button" type="submit" value="Put it in the Pantry" onSubmit={onSubmitHandler} />
     </form>
   </div>
 }
