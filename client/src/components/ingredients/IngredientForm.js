b4fae38c518d38ee8af2aca9001d3bfa52e2c2a4
Tom Low <thomaslsternberg@gmail.com>
2021-02-18 00:25:22 -0500

    built out auto-complete and image feature

--- client/src/components/ingredients/IngredientForm.js
+++ client/src/components/ingredients/IngredientForm.js
@@ -1,42 +1,25 @@
 import React, { useState } from "react"
-import { Redirect } from "react-router-dom"
 import AsyncSelect from 'react-select/async';
 
 const IngredientForm = (props) => {
+  const [inputValue, setInputValue] = useState('');
+  const [selectedValue, setSelectedValue] = useState({
+    name: "",
+    image: ""
+  });
 
-  // const SelectAsync = () => {
-  //   const [inputValue, setInputValue] = useState('');
-  //   const [selectedValue, setSelectedValue] = useState(null);
-  //   const handleInputChange = value => {
-  //     setInputValue(value)
-  //   };
-  //   const handleChange = value => {
-  //     setSelectedValue(value)
-  //   }
-  //   const loadOptions = (inputValue) => {
-  //     return fetch(`/api/v1/ingredients/?query=${inputValue}`).then(res => res.json())
-  //   };
-  //   return (
-  //     <div>
-  //       <AsyncSelect
-  //         value={selectedValue}
-  //         getOptionLabel={e => e.name}
-  //         getOptionValue={e => e.id}
-  //         loadOptions={loadOptions}
-  //         onInputChange={handleInputChange}
-  //         onChange={handleChange}
-  //       />
-  //       <pre>Selected Value: {JSON.stringify(selectedValue || {}, null, 2)}</pre> (this just shows your returned data for testing)
-  //     </div>
-  //   );
-  // }
-  // export default SelectAsync;
-
-  const [ingredientRecord, setIngredientRecord] = useState({
-    name: ""
-  })
-  const [shouldRedirect, setShouldRedirect] = useState(false)
   const [errors, setErrors] = useState([])
+
+
+  const handleInputChange = value => {
+    setInputValue(value)
+  };
+  const handleChange = value => {
+    setSelectedValue(value)
+  }
+  const loadOptions = (inputValue) => {
+    return fetch(`/api/v1/ingredients/autocomplete?query=${inputValue}`).then(res => res.json())
+  };
 
   const postIngredient = async (formPayload) => {
     const response = await fetch('/api/v1/ingredients', {
@@ -57,34 +40,30 @@ const IngredientForm = (props) => {
         throw error
       }
     }
-
     else {
-      setIngredientRecord({ name: "" })
+      setSelectedValue({ name: "", image: "" })
       setErrors([])
-      setShouldRedirect(true)
     }
-  }
-
-  const handleInputChange = (event) => {
-    setIngredientRecord({
-      ...ingredientRecord,
-      [event.currentTarget.name]: event.currentTarget.value
-    })
   }
 
   const onSubmitHandler = (event) => {
     event.preventDefault()
-    postIngredient(ingredientRecord)
-  }
-
-  if (shouldRedirect) {
-    return <Redirect to="/pantry" />
+    postIngredient(selectedValue)
   }
 
   return <div>
     <form onSubmit={onSubmitHandler}>
       <label>Ingredient Name:
-        <input type="text" id="name" name="name" value={ingredientRecord.name} onChange={handleInputChange} />
+        <div>
+          <AsyncSelect
+            value={selectedValue}
+            getOptionLabel={e => e.name}
+            getOptionValue={e => e.name}
+            loadOptions={loadOptions}
+            onInputChange={handleInputChange}
+            onChange={handleChange}
+          />
+        </div>
       </label >
       <input className="button" type="submit" value="Put it in the Pantry" onSubmit={onSubmitHandler} />
     </form >
--- client/src/components/ingredients/IngredientListItem.js
+++ client/src/components/ingredients/IngredientListItem.js
@@ -1,23 +1,23 @@
 import React from "react"
 import { Link } from "react-router-dom"
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { faEdit, faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEdit, faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 
 const IngredientListItem = ({ ingredient, index }) => {
   return <div className="ingredient-list-item">
-    <div className="ingredient-left-column">
-      <p key={index}>{ingredient.name}</p>
+    <div className="ingredient-right-column">
+      <img src={`https://spoonacular.com/cdn/ingredients_100x100/${ingredient.image}`} alt="an api-provided image for this ingredient" />
+    </div>
+    <div className="ingredient-left-column text-center">
+      <p key={index} className="ingredient-name">{ingredient.name}</p>
       <div className="ingredient-icons">
         <Link to={`/ingredients/edit/${ingredient.id}`}>
-          {/* <FontAwesomeIcon icon={"faEdit"} /> */}
+          <FontAwesomeIcon icon={faEdit} className="ingredient-icon" />
         </Link>
         <Link to={`/ingredients/delete/${ingredient.id}`}>
-          {/* <FontAwesomeIcon icon={"faTrashAlt"} /> */}
+          <FontAwesomeIcon icon={faTrashAlt} className="ingredient-icon" />
         </Link>
       </div>
-    </div>
-    <div className="ingredient-right-column">
-      <img src={`https://spoonacular.com/cdn/ingredients_100x100/apple.jpg`} alt="an api-provided image for this ingredient" />
     </div>
   </div>
 }
