aaff723ffdb6960978f8650513dc7938345386e4
Tom Low <thomaslsternberg@gmail.com>
2021-05-03 00:12:16 -0400

    rephrasing

--- client/src/components/layout/GuidePage.js
+++ client/src/components/layout/GuidePage.js
@@ -7,11 +7,11 @@ const GuidePage = (props) => {
     <div className="guide-container">
       <h2>New to DinnerBell?</h2>
       <div className="guide-points">
-        <p>Hi! This is a kitchen tracking app that lets you enter ingredients in your pantry and search recipes you can make with them.</p>
+        <p>Hi! This is a kitchen tracking app that lets you enter ingredients in your pantry and search recipes you can make with them. You can even click on the recipes to see instructions and missing ingredients!</p>
 
         <p>I recommend you <Link to="/users/new">sign up</Link> for the best experience—that'll let you save recipes to your profile for later.</p>
 
-        <p>If you're just looking to figure out tonight (as I was when I built this app), feel free to go ahead with the simple search <Link to="/pantry">here</Link>.</p>
+        <p>If you're just looking to get something on the table tonight (as I was when I built this app), feel free to go ahead with the simple search <Link to="/pantry">here</Link>.</p>
       </div>
 
       <h2>Meet the Creator</h2>
